93df3da170864d7289bde685975be1b2499deac9
SimoneZampieri <simonezampieri106@gmail.com>
2025-01-23 13:50:17 +0100

    [update] added nav

---
+++ src/components/Nav.jsx
@@ -0,0 +1,21 @@
+import { NavLink } from "react-router-dom";
+
+function Nav = () =>{
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <NavLink to="/">Home</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/about">Chi Siamo</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/posts">Lista Post</NavLink>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
