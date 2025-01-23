eb01bff5f74f7aae8fa49e558f49e0247010a5cd
SimoneZampieri <simonezampieri106@gmail.com>
2025-01-23 13:52:34 +0100

    [update] refined layout

--- src/layouts/DefLayout.jsx
+++ src/layouts/DefLayout.jsx
@@ -1,7 +1,17 @@
 import { Outlet } from "react-router-dom";
+import Nav from "../components/Nav"
 
 function DefLayout = () =>{
     return(
-        <></>
+        <>
+            <header>
+                <Nav/>
+            </header>
+            <main>
+                <Outlet />
+            </main>
+        </>
     )
 }
+
+export default DefLayout
