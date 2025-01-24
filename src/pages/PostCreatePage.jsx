909e8eae2ac7a92df359404ab06c9375978c0c92
SimoneZampieri <simonezampieri106@gmail.com>
2025-01-24 12:14:17 +0100

    [update] created logic in post create page

--- src/pages/PostCreatePage.jsx
+++ src/pages/PostCreatePage.jsx
@@ -12,4 +12,27 @@ const PostCreate = () => {
   const navigate = useNavigate();
   const baseApiUrl = import.meta.env.VITE_BASE_API_URL;
   const [formData, setFormData] = useState(initialFormData);
+
+  const inputHandler = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const tags = formData.tags.split(",").map((tag) => tag.trim());
+    const newPost = { ...formData, tags: tags };
+    axios
+      .post(`${baseApiUrl}/posts`, newPost)
+      .then((res) => {
+        fetchPosts();
+        setFormData(initialFormData);
+      })
+      .catch((error) => {
+        console.error("Error creating post:", error);
+      });
+  };
 };
