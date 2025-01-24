bf03fe3f18fb809a9548cb1d18f0216738241d57
SimoneZampieri <simonezampieri106@gmail.com>
2025-01-24 11:41:37 +0100

    [update] created and defined error page

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,11 @@
+const ErrorPage = () => {
+  return (
+    <>
+      <div className="container my-3 d-flex justify-content-center">
+        ERROR 404 NOT FOUND
+      </div>
+    </>
+  );
+};
+
+export default ErrorPage;
