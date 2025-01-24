24da8646da8e75bba4e1fda0f0af33cfccef0918
SimoneZampieri <simonezampieri106@gmail.com>
2025-01-24 11:23:39 +0100

    [update] defined post list

--- src/pages/PostListPage.jsx
+++ src/pages/PostListPage.jsx
@@ -1,5 +1,57 @@
-function PostList() {
-  return <h1>Lista Post</h1>;
-}
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+const PostList = () => {
+  const [posts, setPosts] = useState(null);
+  const baseApiUrl = import.meta.env.VITE_BASE_API_URL;
+
+  const fetchPosts = () => {
+    axios
+      .get(`${baseApiUrl}/posts`)
+      .then((res) => {
+        setPosts(res.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching posts:", error);
+      });
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  return (
+    <>
+      <div className="container my-5">
+        <div className="card">
+          <div className="card-body">
+            <h1>Lista Post</h1>
+            <ul className="list-group my-5">
+              {posts === null ? (
+                <h2>Loading...</h2>
+              ) : (
+                posts.map((post) => (
+                  <li
+                    key={post.id}
+                    className="list-group-item d-flex justify-content-between"
+                  >
+                    <span>{post.name}</span>
+                    <Link
+                      className="btn btn-success"
+                      to={`/dettaglio-post/${post.id}`}
+                    >
+                      Vedi dettaglio
+                    </Link>
+                  </li>
+                ))
+              )}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default PostList;
