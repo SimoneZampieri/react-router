b4e2a90c1d4aebe4bf369be391ea547fc49cf78d
SimoneZampieri <simonezampieri106@gmail.com>
2025-01-24 11:30:01 +0100

    [update] defined logic in postdetail

--- src/pages/PostDetailPage.jsx
+++ src/pages/PostDetailPage.jsx
@@ -6,4 +6,30 @@ const PostDetail = () => {
   const baseApiUrl = import.meta.env.VITE_BASE_API_URL;
   const [posts, setPosts] = useState(null);
   const navigate = useNavigate();
+
+  const fetchPosts = () => {
+    axios
+      .get(`${baseApiUrl}/posts`)
+      .then((res) => {
+        setPosts(res.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching posts:", error);
+      });
+  };
+
+  const deleteHand = (id) => {
+    axios
+      .delete(`${baseApiUrl}/posts/${id}`)
+      .then(() => {
+        navigate("/posts");
+      })
+      .catch((error) => {
+        console.error("Error deleting post:", error);
+      });
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 };
